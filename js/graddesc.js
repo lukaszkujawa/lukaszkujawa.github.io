61f97e7523393af29f0b16d4473413d2e198b356
Lukasz Kujawa <lukasz.f24@gmail.com>
2015-03-07 22:44:47 +0000

    cleanup / added scale to cost function

--- js/graddesc.js
+++ js/graddesc.js
@@ -21,10 +21,14 @@
 		minCost: 0,
 		maxCost: 0,
 		svg: null,
+		prevPoint: null,
+		xAxis: null,
+		yAxis: null,
 
 		init: function() {
 			
 			this.initScales();
+			this.initAxis();
 
 			this.svg = d3.select("body").append("svg")
 							.attr("width", this.width)
@@ -33,25 +37,53 @@
 			this.xi = (this.width-2*Settings.margin) / this.size;
 			this.yi = (this.height-2*Settings.margin) / this.size;
 
+			this.prevPoint = {
+				theta0: AnimatedFunction.theta0,
+				theta1: AnimatedFunction.theta1
+			};
+
+			
+			this.svg.append("g")
+				.attr("class", "axis x")
+				.attr("transform", "translate(0," + (this.height - Settings.margin) + ")")
+				.call(this.xAxis);
+
+
+			this.svg.append("g")
+				.attr("class", "axis y")
+				.attr("transform", "translate(" + Settings.margin + ",0)")
+				.call(this.yAxis);
+			
 		},
 
 		initScales: function() {
 			this.xNorm = d3.scale.linear()
-				.domain([0, this.width])
+				.domain([Settings.margin , this.width - Settings.margin ])
 				.range([-3, 3]);
 
 			this.yNorm = d3.scale.linear()
-				.domain([0, this.height])
+				.domain([Settings.margin , this.height - Settings.margin ])
 				.range([-3, 3]);
 
 			this.xDeNorm = d3.scale.linear()
 				.domain([-3, 3])
-				.range([0, this.width]);
+				.range([Settings.margin , this.width - Settings.margin ]);
 
 			this.yDeNorm = d3.scale.linear()
 				.domain([-3, 3])
-				.range([0, this.height]);
+				.range([Settings.margin , this.height - Settings.margin]);
 		},
+
+		initAxis: function() {
+			this.xAxis = d3.svg.axis()
+				.scale(this.xDeNorm)
+				.orient("bottom");
+
+			this.yAxis = d3.svg.axis()
+				.scale(this.yDeNorm)
+				.orient("left");
+		},
+
 
 		getCost: function(theta0, theta1) {
 			var cost = 0;
@@ -90,19 +122,38 @@
 				return;
 			}
 
+			var x1 = (CostFunction.xDeNorm(this.prevPoint.theta0)),
+				y1 = (CostFunction.yDeNorm(this.prevPoint.theta1)),
+				x2 = (CostFunction.xDeNorm(AnimatedFunction.theta0)),
+				y2 = (CostFunction.yDeNorm(AnimatedFunction.theta1));
+
+			var dist = Math.pow(x1-x2,2) + Math.pow(y1-y2,2);
+
+			if(dist < 1) {
+				return;
+			}
+
 			var point = this.svg.selectAll("point").data([{
 							theta0: AnimatedFunction.theta0, 
 							theta1: AnimatedFunction.theta1}]);
 
+			var g = point.enter().append("g");
+			g.append("line")
+				.attr("stroke", "red")
+				.attr("x1", Math.round(x1))
+				.attr("y1", Math.round(y1))
+				.attr("x2", function(d){return Math.round(x2);})
+				.attr("y2", function(d){return Math.round(y2);});
 
-			var p = [point, point.enter().append("circle")];
-			for(i in p) {
-				p[i]
-					.attr("cx", function(d){ return Math.round(CostFunction.xDeNorm(d.theta0)); })
-					.attr("cy", function(d){ return Math.round(CostFunction.yDeNorm(d.theta1)); })
-					.attr("r", "2")
-					.attr("fill", "red");
-			}
+			g.append("circle")
+				.attr("cx", Math.round(x2))
+				.attr("cy", Math.round(y2))
+				.attr("r", "1.4")
+				.attr("fill", "red");
+
+			this.prevPoint = {
+							theta0: AnimatedFunction.theta0, 
+							theta1: AnimatedFunction.theta1};
 		},
 
 		draw: function(svg2) {
@@ -161,7 +212,7 @@
 
 	var AnimatedFunction = {
 		theta0: 0,
-		theta1: 1,
+		theta1: 0,
 		learnRate: 0.07,
 		xDenorm: null,
 		yDenorm: null,
@@ -393,7 +444,7 @@
 			for(i = 0 ; i < 100 ; i++) {
 				AnimatedFunction.iterateTheta();
 			}
-			//console.log(AnimatedFunction.theta1 + " " + AnimatedFunction.theta0);
+			
 			AnimatedFunction.draw(GradDesc.svg);
 			CostFunction.animatePointer();
 		}
